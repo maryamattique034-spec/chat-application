1366c8aeaf1c9af7f7869cda28a850913e4be773
maryamattique034-spec <maryamattique034@gmail.com>
2025-10-29 19:47:11 +0500

    added msg timestamps and room_list with auth

--- chat/models.py
+++ chat/models.py
@@ -9,3 +9,9 @@ class Message(models.Model):
 
     def __str__(self):
         return f"{self.user}: {self.message[:20]}"
+
+class Room(models.Model):
+    name = models.CharField(max_length=100, unique = True)
+
+    def __str__(self):
+        return self.name
--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, redirect
-from .models import Message
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Message, Room
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
@@ -10,8 +10,14 @@ from django.contrib.auth.decorators import login_required
 #         'room_name': room_name
 #     })
 
+
+def room_list(request):
+    rooms = Room.objects.all()
+    return render(request, 'chat/room-list.html', {'rooms': rooms})
+
 @login_required(login_url = 'login')
 def room(request,room_name,):
+    room = get_object_or_404(Room,name = room_name)
     messages = Message.objects.filter(room_name=room_name).order_by('timestamp')
     return render(request, 'chat/room.html',{
         'room_name': room_name,
@@ -20,14 +26,14 @@ def room(request,room_name,):
 
 def user_login(request):
     if request.user.is_authenticated:
-        return redirect('room', room_name= 'general')
+        return redirect('room-list')
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(request,username=username , password=password)
         if user is not None:
             login(request,user)
-            return redirect('room', room_name = 'general')
+            return redirect('room-list')
         else:
             logout(request)
             messages.error(request, 'Invalid username or password')
